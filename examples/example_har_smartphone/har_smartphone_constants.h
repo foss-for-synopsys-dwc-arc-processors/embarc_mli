136cbb8bacd967952588fa4e72156dc77f1d9d7d
Denis Yudakov <43009322+dyudakov@users.noreply.github.com>
2019-03-07 18:46:08 +0300

    Gnu har example (#9)
    * updated example har smartphone for GNU toolchain
    
    * fixed link and cmd line
    
    * updated README.md
    
    * updated Makefile - renamed PUBLIC_DIR variable. added creation of a bin directory for external lib.
    
    * updated README.md - Added some Notes for GNU part
    
    * fixed link to specific paragraph
    
    * added folder for prebuilt library, added README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- examples/example_har_smartphone/har_smartphone_constants.h
+++ examples/example_har_smartphone/har_smartphone_constants.h
@@ -34,7 +34,6 @@
 #include "har_smartphone_model.h"
 #include "tests_aux.h"
 
-
 // Defining weight data type
 //===================================
 #if (MODEL_BIT_DEPTH != MODEL_FX_16)
@@ -45,10 +44,29 @@ typedef int8_t w_type;
 typedef int16_t w_type;
 #endif
 
-
 // Defining data sections attributes
 //===================================
 #if (ARC_PLATFORM == V2DSP_XY)
+#if defined (__GNUC__) && !defined (__CCAC__)
+//ARC GNU tools
+// Model Weights attribute
+#define _Wdata_attr __attribute__((section(".mli_model")))
+#define _W  _Wdata_attr
+
+// Bank X (XCCM) attribute
+#define __Xdata_attr __attribute__((section(".Xdata")))
+#define _X  __Xdata_attr
+
+// Bank Y (YCCM) attribute
+#define __Ydata_attr __attribute__((section(".Ydata")))
+#define _Y  __Ydata_attr
+
+// Bank Z (DCCM) attribute
+#define __Zdata_attr __attribute__((section(".Zdata")))
+#define _Z  __Zdata_attr
+
+#else	
+//Metaware tools
 // Model Weights attribute
 #define _Wdata_attr __attribute__((section(".mli_model")))
 #define _W __xy _Wdata_attr
@@ -64,6 +82,7 @@ typedef int16_t w_type;
 // Bank Z (DCCM) attribute
 #define __Zdata_attr __attribute__((section(".Zdata")))
 #define _Z __xy __Zdata_attr
+#endif // if defined (__GNUC__) && !defined (__CCAC__)
 
 #else
 #define _X __attribute__((section(".mli_ir_buf")))
@@ -123,7 +142,6 @@ extern const w_type  _W  L4_fc_bias_buf[];
 #define FC4_W_INT   (0)
 #define FC4_B_INT   (-2)
 #define FC4_OUT_INT (3)
-
 
 //======================================================
 //
