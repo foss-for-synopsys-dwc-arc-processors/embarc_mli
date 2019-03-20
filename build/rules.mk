81482b1b99106b338688cb2090b87f63509fae6a
Daniil Demidov <ddemido@us01dwemt376.internal.synopsys.com>
2019-03-20 10:09:52 -0700

    always prepend backslash before quote (on Win too)

--- build/rules.mk
+++ build/rules.mk
@@ -9,13 +9,17 @@
 #=============================================================
 # OS-specific definitions
 #=============================================================
+COMMA=,
+OPEN_PAREN=(
+CLOSE_PAREN=)
+BACKSLASH=\$(nullstring)
 ifneq ($(ComSpec)$(COMSPEC),)
     O_SYS=Windows
     RM=del /F /Q
     MKDIR=mkdir 
     CP=copy /Y
     TYPE=type
-    PS=\$(nullstring)
+    PS=$(BACKSLASH)
     Q=
     coQ=\$(nullstring)
     fix_platform_path = $(subst /,$(PS), $(1))
@@ -27,15 +31,19 @@ else
     CP=cp 
     TYPE=cat
     PS=/
-    Q=\$(nullstring)
-    coQ=^
+    Q=$(BACKSLASH)
+    coQ=
     fix_platform_path=$(1)
     DEV_NULL=/dev/null
 endif
 
-COMMA=,
-OPEN_PAREN=(
-CLOSE_PAREN=)
+# Note: Windows escaping rules is very combersome 
+# initially I tried to use Q=^, but this depends on the context and (looks like) on Win version.
+# Also expecially ugly thing is that in quoted strings the quotes the same are remain.
+# Batch has special parameter expansion syntax to remove quotes,
+# but many tools themselves remove quotes (unless escaped with backslash)
+# So finally we've found that in our use cases we may not escaping any symbols but prepend backslashes before quotes.
+
 quote=$(subst $(coQ),$(Q)$(coQ), \
       $(subst %,$(Q)%, \
       $(subst &,$(Q)&, \
@@ -48,7 +56,7 @@ quote=$(subst $(coQ),$(Q)$(coQ), \
       $(subst $(OPEN_PAREN),$(Q)$(OPEN_PAREN), \
       $(subst $(CLOSE_PAREN),$(Q)$(CLOSE_PAREN), \
       $(subst !,$(Q)!, \
-      $(subst ",$(Q)$(coQ)", \
+      $(subst ",$(BACKSLASH)", \
       $(subst $(Q),$(Q)$(Q), \
 	  $(1) ))))))))))))))
 
