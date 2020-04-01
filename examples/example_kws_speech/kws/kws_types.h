f9ce67c137990d9d48b9b99eb30f099e11125b9f
Daria Zhuravleva <daria@synopsys.com>
2020-04-01 14:13:22 +0300

    Added copyright

--- examples/example_kws_speech/kws/kws_types.h
+++ examples/example_kws_speech/kws/kws_types.h
@@ -1,3 +1,12 @@
+/*
+* Copyright 2019, Synopsys, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-3-Clause license found in
+* the LICENSE file in the root directory of this source tree.
+*
+*/
+
 #ifndef _KWS_MODULE_TYPES_H_
 #define _KWS_MODULE_TYPES_H_
 
