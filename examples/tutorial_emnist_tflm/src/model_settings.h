362411fa8e0ce844a309bbba4f8bac9895b444f0
daria <daria@synopsys.com>
2020-06-24 20:48:06 +0300

    changed example structure; added makefile

--- examples/tutorial_emnist_tflm/src/model_settings.h
+++ examples/tutorial_emnist_tflm/src/model_settings.h
@@ -1,3 +1,12 @@
+/*
+* Copyright 2019-2020, Synopsys, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-3-Clause license found in
+* the LICENSE file in the root directory of this source tree.
+*
+*/
+
 #ifndef MODEL_SETTINGS_H_
 #define MODEL_SETTINGS_H_
 
---
+++ examples/tutorial_emnist_tflm/src/test_samples.h
@@ -0,0 +1,19 @@
+/*
+* Copyright 2019-2020, Synopsys, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-3-Clause license found in
+* the LICENSE file in the root directory of this source tree.
+*
+*/
+
+#include <cstdint>
+#include "model_settings.h"
+
+struct TestSample {
+  uint8_t image[kImageSize];
+  int label;
+};
+
+extern const int kNumSamples;
+extern const TestSample test_samples[];
