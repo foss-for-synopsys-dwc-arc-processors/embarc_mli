42c9d4fef94499c297a475218df95d08f6ee34ac
kiyaev <kiyaev@synopsys.com>
2020-07-27 10:50:51 -0700

    [vpx] build support for VPX
    [Change]
    - build support for VPX

---
+++ lib/src/kernels/eltwise_vpx/mli_krn_eltwise_add_fx.cc
@@ -0,0 +1,14 @@
+/*
+* Copyright 2020-2020, Synopsys, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-3-Clause license found in
+* the LICENSE file in the root directory of this source tree.
+*
+*/
+
+#include "stdio.h"
+
+void test() {
+    printf("test\n");
+}
