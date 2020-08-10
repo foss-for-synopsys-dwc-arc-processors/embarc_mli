60cc6099cfac7f2ba63551ea01bde7f766cd58c2
jacco <jacco@synopsys.com>
2020-08-10 18:17:16 +0200

    add 4x10 special conv kernel

--- lib/src/kernels/convolution/mli_krn_conv2d_chw_fx8.cc
+++ lib/src/kernels/convolution/mli_krn_conv2d_chw_fx8.cc
@@ -1009,6 +1009,104 @@ mli_status mli_krn_conv2d_chw_fx8_k4x4_ch1_str1_krnpad(
     return MLI_STATUS_OK;
 }
 
+mli_status mli_krn_conv2d_chw_fx8_k4x10_ch1_str1_krnpad(
+        const mli_tensor * in, 
+        const mli_tensor * weights, 
+        const mli_tensor * bias, 
+        const mli_conv2d_cfg * cfg, 
+        mli_tensor * out) {
+    mli_status ret = MLI_CHECK_STATUS(mli_chk_conv2d_chw_fx8(in, weights, bias, cfg, out), __func__);
+    if (ret != MLI_STATUS_OK)
+        return ret;
+
+    // Extract general conv2D parameters
+    int stride_width = cfg->stride_width;
+    int stride_height = cfg->stride_height;
+    int padding_top = cfg->padding_top;
+    int padding_bot = cfg->padding_bottom;
+    int padding_left = cfg->padding_left;
+    int padding_right = cfg->padding_right;
+    int kernel_height = weights->shape[KRNL_H_DIM_CHW];
+    int kernel_width = weights->shape[KRNL_W_DIM_CHW];
+    int in_ch = in->shape[FMAP_C_DIM_CHW];
+
+    // assign hard coded values for this variation to some variables
+#if 1
+    MLI_CHECK_AND_FIX(stride_width, 1);
+#endif
+#if 1
+    MLI_CHECK_AND_FIX(stride_height, 1);
+#endif
+#if 1
+    MLI_CHECK_AND_FIX(padding_top, 4);
+    MLI_CHECK_AND_FIX(padding_bot, 5);
+    MLI_CHECK_AND_FIX(padding_left, 1);
+    MLI_CHECK_AND_FIX(padding_right, 2);
+#endif
+#if 4
+    MLI_CHECK_AND_FIX(kernel_width, 4);
+#endif
+#if 4
+    MLI_CHECK_AND_FIX(kernel_height, 10);
+#endif
+#if 1
+    MLI_CHECK_AND_FIX(in_ch, 1);
+#endif
+
+    mli_minmax_t val_limit;
+    // fill output tensor el_type parameter
+    out->el_type = MLI_EL_FX_8;
+    // Define output val limits - we need it in case built-in RELU
+    val_limit = mli_prv_get_relu_min_max(&cfg->relu, out);
+
+    // Data pointers
+    MLI_PTR(int8_t) in_ftrs = (MLI_PTR(int8_t ))in->data;
+    MLI_CONV_OUT_PTR(int8_t) out_ftrs = (MLI_CONV_OUT_PTR(int8_t ))out->data;
+    MLI_PTR(int8_t) wt = (MLI_PTR(int8_t ))weights->data;
+    MLI_PTR(int8_t) bs = (MLI_PTR(int8_t ))bias->data;
+
+    // Define Data dimensions
+    int out_ch = weights->shape[KRNL_C_DIM_CHW];
+
+    int in_height = in->shape[FMAP_H_DIM_CHW];
+    int in_width = in->shape[FMAP_W_DIM_CHW];
+
+    int out_width = CEIL_DIV(in_width + padding_left + padding_right - kernel_width + 1, stride_width);
+    int out_height = CEIL_DIV(in_height + padding_top + padding_bot - kernel_height + 1, stride_height);
+
+    // Define shift values
+    int bias_shift = mli_prv_calc_shift(in, weights, bias);
+    int out_shift = mli_prv_calc_shift(in, weights, out);
+
+    //=======================================================================
+    rect_t cent_area;
+    cent_area.row_beg = 0;
+    cent_area.row_end = out_height;
+    cent_area.clmn_beg = 0;
+    cent_area.clmn_end = out_width;
+
+    mli_prv_fx_init_dsp_ctrl();
+
+    conv2d_chw_str1(
+        in_ftrs, wt, bs, out_ftrs, &cent_area,
+        bias_shift, out_shift,
+        val_limit.min, val_limit.max,
+        in_ch, in_width, in_height,
+        out_ch, out_width, out_height,
+        kernel_height, kernel_width,
+        stride_height, stride_width,
+        padding_top, padding_bot, padding_left, padding_right,
+        1, 0);
+
+    // fill output tensor parameters
+    out->rank = in->rank;
+    out->shape[FMAP_C_DIM_CHW] = out_ch;
+    out->shape[FMAP_H_DIM_CHW] = out_height;
+    out->shape[FMAP_W_DIM_CHW] = out_width;
+
+    return MLI_STATUS_OK;
+}
+
 mli_status mli_krn_conv2d_chw_fx8_k5x5_str1_krnpad(
         const mli_tensor * in, 
         const mli_tensor * weights, 
